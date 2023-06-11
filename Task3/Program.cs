989c1e37a4053936ee0fbd75c1e721a179270fe1
Boris <89241300089@mail.ru>
2023-06-11 12:53:53 +0900

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+class Program {
+    public static void Main(string[] args) {
+        int[,] matrix = GetArray();
+        PrintMatrix(matrix);
+        Console.Write("Среднее арифметическое каждого столбца: " + SearchAverage(matrix));
+    }
+
+    static int[,] GetArray() {
+        Random rand = new Random();
+        int[,] arr = new int[rand.Next(2, 11), rand.Next(2, 11)];
+        FillArray(arr);
+        return arr;
+    }
+    
+    static void FillArray(int[,] arr) {
+        Random rand = new Random();
+        for (int i = 0; i < arr.GetLength(0); i++) {
+            for (int j = 0; j < arr.GetLength(1); j++) {
+                arr[i,j] = rand.Next(10);
+            }
+        }
+    }
+
+    static void PrintMatrix(int[,] array) {
+        for (int i = 0; i < array.GetLength(0); i++) {
+            for (int j = 0; j < array.GetLength(1); j++) {
+                Console.Write(array[i,j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static string SearchAverage(int[,] array) {
+        string result = string.Empty;
+        double sum = 0;
+        int lengthRow = array.GetLength(0);
+        int lengthCol = array.GetLength(1);
+        for (int j = 0; j < lengthCol; j++) {
+            sum = 0;
+            for (int i = 0; i < lengthRow; i++) {
+                sum += array[i,j];
+            }
+            result += Math.Round(sum/lengthRow,2) + (j < lengthCol - 1 ? "; " : ".");
+        }
+        return result;
+    }
+}
